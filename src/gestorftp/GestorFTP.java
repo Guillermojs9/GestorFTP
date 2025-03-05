e71dd094c40c9197880b96f80a3406ca4f3b109b
Guillermo <grobles64@ieszaidinvergeles.org>
2025-03-05 13:45:37 +0100

    Sesión 1. Funcionamiento básico. Subir y descargar.

--- src/gestorftp/GestorFTP.java
+++ src/gestorftp/GestorFTP.java
@@ -9,18 +9,18 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.PrintStream;
-import java.io.UnsupportedEncodingException;
 import org.apache.commons.net.ftp.FTP;
 import org.apache.commons.net.ftp.FTPClient;
 import org.apache.commons.net.ftp.FTPReply;
 
 public class GestorFTP {
 
+    //172.26.157.64
     private FTPClient clienteFTP;
-    private static final String SERVIDOR = "localhost";
+    private static final String SERVIDOR = "172.26.157.64";
     private static final int PUERTO = 21;
     private static final String USUARIO = "guillermo";
-    private static final String PASSWORD = "1234";
+    private static final String PASSWORD = "guillermo";
 
     public GestorFTP() {
         clienteFTP = new FTPClient();
@@ -59,6 +59,7 @@ public class GestorFTP {
             InputStream is = new FileInputStream(ficheroLocal);
             boolean enviado = clienteFTP.storeFile(ficheroLocal.getName(), is);
             is.close();
+            System.out.println("Enviado: " + enviado);
             exito = enviado;
         } catch (FileNotFoundException ex) {
             System.out.println("Error al buscar el archivo. Excepción: " + ex.getMessage());
@@ -69,28 +70,60 @@ public class GestorFTP {
     }
 
     private boolean descargarFichero(String ficheroRemoto, String pathLocal) {
-        boolean exito = false;
-        try {
-            OutputStream os = new BufferedOutputStream(new FileOutputStream(pathLocal));
-            boolean recibido = clienteFTP.retrieveFile(ficheroRemoto, os);
-            os.close();
-            exito = recibido;
-        } catch (FileNotFoundException ex) {
-            System.out.println("Error al buscar el archivo. Excepción: " + ex.getMessage());
-        } catch (IOException ex) {
-            System.out.println("Error al descargar el archivo. Excepción: " + ex.getMessage());
+    boolean exito = false;
+    try {
+        File localDir = new File(pathLocal);
+        if (!localDir.exists()) {
+            boolean dirCreated = localDir.mkdirs();
+            if (!dirCreated) {
+                System.out.println("No se pudo crear el directorio local: " + pathLocal);
+                return false;
+            }
         }
-        return exito;
+        File localFile = new File(localDir, ficheroRemoto);
+        OutputStream os = new BufferedOutputStream(new FileOutputStream(localFile));
+        boolean recibido = clienteFTP.retrieveFile(ficheroRemoto, os);
+        System.out.println("Respuesta del servidor: " + clienteFTP.getReplyString());
+        System.out.println("Descargado: " + recibido);
+        
+        os.close();
+        exito = recibido;
+    } catch (FileNotFoundException ex) {
+        System.out.println("Error, archivo no encontrado. Excepción: " + ex.getMessage());
+    } catch (IOException ex) {
+        System.out.println("Error durante la descarga. Excepción: " + ex.getMessage());
     }
+    return exito;
+}
 
     public static void main(String[] args) {
         try {
             System.setOut(new PrintStream(System.out, true, "UTF-8"));
+            String fichero_descarga = "test.txt";
 
-        } catch (UnsupportedEncodingException ex) {
-            System.out.println("Error al intentar codificar: Excepción: " + ex.getMessage());
+            GestorFTP gestorFTP = new GestorFTP();
+            gestorFTP.conectar();
+            System.out.println("Conectado");
+            /*
+            boolean subido = gestorFTP.subirFichero("C:/Users/guillermo/Desktop/test.txt");
+            if (subido) {
+                System.out.println("Fichero subido correctamente");
+            } else {
+                System.err.println("Ha ocurrido un error al intentar subir el fichero");
+            }
+
+             */
+            boolean descargado = gestorFTP.descargarFichero(fichero_descarga, "C:/ftpPrueba2");
+            if (descargado) {
+                System.out.println("Fichero descargado correctamente");
+            } else {
+                System.err.println("Ha ocurrido un error al intentar descargar el fichero.");
+            }
+
+            gestorFTP.desconectar();
+            System.out.println("Desconectado");
+        } catch (Exception e) {
+            System.err.println("Ha ocurrido un error:" + e.getMessage());
         }
-
     }
-
 }
